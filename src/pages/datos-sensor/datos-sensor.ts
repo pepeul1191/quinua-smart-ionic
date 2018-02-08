ee5f290274e6a901680dd01d92af6663ccbaa283
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-08 09:55:12 -0500

    Reporte de max, min y avg de sensor

--- src/pages/datos-sensor/datos-sensor.ts
+++ src/pages/datos-sensor/datos-sensor.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { reporte_sensor } from "../../app/data";
 import { ToastController } from 'ionic-angular';
 import { SensorProvider } from '../../providers/sensor/sensor';
+import { Chart } from 'chart.js';
 
 @IonicPage()
 @Component({
@@ -10,12 +11,14 @@ import { SensorProvider } from '../../providers/sensor/sensor';
   templateUrl: 'datos-sensor.html',
 })
 export class DatosSensorPage {
+  @ViewChild('reporteCanvas') reporteCanvas;
   public info_vista;
   fecha_inicio:any;
   fecha_fin:any;
   sensor_id:any;
   reporte_id:any;
   reporte_url:any;
+  reporteGrafica: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public sensorProvider: SensorProvider) {
     this.sensor_id = navParams.get('sensor_id');
@@ -33,18 +36,106 @@ export class DatosSensorPage {
   }
 
   reporte(tipo) {
-        try {
-            this.sensorProvider.obtener_datos(this.sensor_id, this.reporte_url, this.fecha_inicio, this.fecha_fin).then((rpta) => {
-                console.log("rpta : " + rpta);
-            });
+    try {
+      this.sensorProvider.obtener_datos(this.sensor_id, this.reporte_url, this.fecha_inicio, this.fecha_fin).then((data) => {
+        var data_chart = [];
+        var axis_x = [];
+        var data_max = [];
+        var data_min = [];
+        var data_avg = [];
+        console.log(data);
+        if(data instanceof Array){
+          for (let entry of data) {
+            //data_chart.push(entry['dato']);
+            axis_x.push(entry['dia']);
+            data_max.push(entry['maximo']);
+            data_min.push(entry['minimo']);
+            data_avg.push(entry['promedio']);
+          }
         }
-        catch (e){
-            //console.log(e);
-            let toast = this.toastCtrl.create({
-                message: 'Ocurrió un error en mostrar los datos',
-                duration: 3000
-            });
-            toast.present();
-        }
+        this.reporteGrafica = new Chart(this.reporteCanvas.nativeElement, {
+          type: 'bar',
+          data: {
+            labels: axis_x,
+            datasets: [
+              {
+                label: "Máximo",
+                data: data_max,
+                backgroundColor: [
+                  "rgba(10,20,30,0.3)",
+                  "rgba(10,20,30,0.3)",
+                  "rgba(10,20,30,0.3)",
+                  "rgba(10,20,30,0.3)",
+                  "rgba(10,20,30,0.3)"
+                ],
+                borderColor: [
+                  "rgba(10,20,30,1)",
+                  "rgba(10,20,30,1)",
+                  "rgba(10,20,30,1)",
+                  "rgba(10,20,30,1)",
+                  "rgba(10,20,30,1)"
+                ],
+                borderWidth: 1
+              },
+              {
+                label: "Mínimo",
+                data: data_min,
+                backgroundColor: [
+                  "rgba(50,150,200,0.3)",
+                  "rgba(50,150,200,0.3)",
+                  "rgba(50,150,200,0.3)",
+                  "rgba(50,150,200,0.3)",
+                  "rgba(50,150,200,0.3)"
+                ],
+                borderColor: [
+                  "rgba(50,150,200,1)",
+                  "rgba(50,150,200,1)",
+                  "rgba(50,150,200,1)",
+                  "rgba(50,150,200,1)",
+                  "rgba(50,150,200,1)"
+                ],
+                borderWidth: 1
+              },
+              {
+                label: "Promedio",
+                data: data_avg,
+                backgroundColor: [
+                  "rgba(50,150,100,0.3)",
+                  "rgba(50,150,100,0.3)",
+                  "rgba(50,150,100,0.3)",
+                  "rgba(50,150,100,0.3)",
+                  "rgba(50,150,100,0.3)"
+                ],
+                borderColor: [
+                  "rgba(50,150,100,1)",
+                  "rgba(50,150,100,1)",
+                  "rgba(50,150,100,1)",
+                  "rgba(50,150,100,1)",
+                  "rgba(50,150,100,1)"
+                ],
+                borderWidth: 1
+              }
+            ]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero:true
+                }
+              }]
+            }
+          }
+        });
+      });
     }
+    catch (e){
+      //console.log(e);
+      let toast = this.toastCtrl.create({
+      message: 'Ocurrió un error en mostrar los datos',
+      duration: 3000
+      });
+      toast.present();
+    }
+  }
 }
